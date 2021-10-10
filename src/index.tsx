8f0ec9f30fb5154866067e83c57854c75f1d4372
Siarhei Kvachonak <s.kvachenok@syberry.com>
2021-10-10 13:00:48 +0300

    task-7: Add error handlings for authorization

--- src/index.tsx
+++ src/index.tsx
@@ -13,8 +13,19 @@ axios.interceptors.response.use(
     return response;
   },
   function(error) {
+    console.log('err', error.response)
     if (error?.response?.status === 400) {
       alert(error.response.data?.data);
+    }
+
+    if (error?.response?.status === 401) {
+      console.log(error?.response)
+      alert('Unauthorized');
+    }
+
+    if (error?.response?.status === 403) {
+         console.log(error?.response)
+      alert('The access is forbidden');
     }
 
     return Promise.reject(error?.response ?? error);
