52f11a84ef4d0bfc2dc37b332e0e2418341e57c0
Siarhei Kvachonak <s.kvachenok@syberry.com>
2021-10-25 23:59:59 +0300

    task-8: Update api route

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://bg552ona8f.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://skvachenok-cart-api-development.eu-west-1.elasticbeanstalk.com',
 };
 
 export default API_PATHS;
