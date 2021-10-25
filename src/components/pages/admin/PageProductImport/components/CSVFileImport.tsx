71a352e066cc697d2e15df508f3581f97bdd62fa
sergey-kvachenok <75844481+sergey-kvachenok@users.noreply.github.com>
2021-10-26 00:00:39 +0300

    task-7: Add error handlings for authorization (#5)

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -29,17 +29,27 @@ export default function CSVFileImport({url, title}: CSVFileImportProps) {
     setFile('');
   };
 
+  const authorization_token = localStorage.getItem('authorization_token');
+console.log(`Basic ${authorization_token}`)
   const uploadFile = async (e: any) => {
+try {
       // Get the presigned URL
       const response = await axios({
         method: 'GET',
         url,
         params: {
           name: encodeURIComponent(file.name)
+        },
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Basic ${authorization_token}`
         }
       })
+    
+
       console.log('File to upload: ', file.name)
       console.log('Uploading to: ', response.data)
+
       const result = await fetch(response.data, {
         method: 'PUT',
         body: file,
@@ -47,10 +57,13 @@ export default function CSVFileImport({url, title}: CSVFileImportProps) {
           "Content-Type": "text/csv"
         }
       })
+
       console.log('Result: ', result)
       setFile('');
+         }catch(error: any) {
+      console.log('ERROR:', error, error.message)
     }
-  ;
+  }
 
   return (
     <div className={classes.content}>
